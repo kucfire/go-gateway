1cb96c045d2921dc1232894772f225c5fe7ccb36
kucfire <cai972316027@qq.com>
2020-10-25 22:47:36 +0800

    rename dirName

---
+++ TCP/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"go-gateway/TCP/unpack"
+	"log"
+	"net"
+)
+
+func main() {
+	// simple tcp server
+	// listen ip:port
+	listener, err := net.Listen("tcp", "localhost:9090")
+	if err != nil {
+		panic(err)
+	}
+
+	// accept client request
+	// create goroutine for each request
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("accept fail,err : %v\n", err)
+			continue
+		}
+
+		go process(conn)
+	}
+}
+
+func process(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		bt, err := unpack.Decode(conn)
+		if err != nil {
+			log.Printf("read from connect fail, err : %v\n", err)
+			break
+		}
+		str := string(bt)
+		fmt.Printf("receive from client, data : %v\n", str)
+	}
+}
