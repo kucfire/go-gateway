e9dbc734912e68ee82574b7c69be6e1eb457d560
kucfire <cai972316027@qq.com>
2020-10-28 16:22:08 +0800

    insert normal dir

---
+++ normal/normalClient/client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"time"
+)
+
+func main() {
+	// create link pool
+	transport := &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second, // 连接超时时间
+			KeepAlive: 30 * time.Second, // 长连接超时时间
+		}).DialContext,
+		MaxIdleConns:          100,              // 最大空闲连接
+		IdleConnTimeout:       90 * time.Second, // 空闲超时时间
+		TLSHandshakeTimeout:   10 * time.Second, // tls握手超时时间
+		ExpectContinueTimeout: time.Second,      // 100-continue状态码超时时间
+	}
+
+	// create a client
+	client := &http.Client{
+		Timeout:   time.Second * 30, // 请求超时时间
+		Transport: transport,
+	}
+
+	// 请求数据
+	resp, err := client.Get("http://localhost:1210/bye")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bds, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bds))
+}
