4f4b612b6e68e4705eedc6066adf2018ad22cb0b
kucfire <cai972316027@qq.com>
2020-11-03 22:57:30 +0800

    update load_balance

--- load_balance/random.go
+++ load_balance/random.go
@@ -1,16 +1,36 @@
 package load_balance
 
-import "errors"
+import (
+	"errors"
+	"math/rand"
+)
 
 type RandomBalance struct {
 	curIndex int
 	rss      []string
-	// conf     LoadBalanceConf
+	conf     LoadBalanceConf
 }
 
+// Add : add addr in RandomBalance.rss
 func (r *RandomBalance) Add(params ...string) error {
 	if len(params) == 0 {
 		return errors.New("param len 1 at least")
 	}
+
+	addr := params[0]
+	r.rss = append(r.rss, addr)
+
 	return nil
+}
+
+func (r *RandomBalance) Get(key string) (string, error) {
+	return r.Next(), nil
+}
+
+func (r *RandomBalance) Next() string {
+	if len(r.rss) == 0 {
+		return ""
+	}
+	r.curIndex = rand.Intn(len(r.rss))
+	return r.rss[r.curIndex]
 }
