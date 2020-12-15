20d7854a64173256a46fb4176513097b17636965
kucfire <cai972316027@qq.com>
2020-12-16 00:04:59 +0800

    update basic

---
+++ circuit_break/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/afex/hystrix-go/hystrix"
+)
+
+func main() {
+	hystrixStreamHandler := hystrix.NewStreamHandler()
+	hystrixStreamHandler.Start()
+	go http.ListenAndServe(":8874", hystrixStreamHandler)
+
+	hystrix.ConfigureCommand("kucfireText", // 第一个参数表示的是熔断器的命名
+		hystrix.CommandConfig{
+			Timeout:                1000, // 单次请求的超时时间，单位mm
+			MaxConcurrentRequests:  1,    // 最大并发量
+			SleepWindow:            5000, // 熔断后多久去尝试服务是否可用，开启状态下尝试服务是否可转为半打开状态的一个间隔窗口，单位mm
+			RequestVolumeThreshold: 10,   // 验证熔断的请求数量，10秒内采样
+			ErrorPercentThreshold:  10,   // 验证熔断的错误百分比，当达到百分百的时候，则进入熔断开启状态
+		})
+
+	for i := 0; i < 10000; i++ {
+		err := hystrix.Do("kucfireText", func() error {
+			// test case 1 并发测试
+			if i == 0 {
+				return errors.New("server error")
+			}
+			// test case 2 超时测试
+			// time.Sleep(2 * time.Second)
+			log.Println("do server")
+			return nil
+		}, nil)
+		if err != nil {
+			log.Println("hystrix err:" + err.Error())
+			time.Sleep(1 * time.Second)
+			log.Println("Sleep 1 Second")
+		}
+	}
+
+	// 监听关闭信号
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-gateway
 
 go 1.15
 
-require golang.org/x/time v0.0.0-20201208040808-7e3f01d25324
+require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324
+)
