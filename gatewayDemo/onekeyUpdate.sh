f9846d97300f79281dfa627029b0865c091424db
kucfire <cai972316027@qq.com>
2021-01-20 09:50:26 +0800

    update main

--- gatewayDemo/onekeyUpdate.sh
+++ gatewayDemo/onekeyUpdate.sh
@@ -7,7 +7,7 @@ export GOPROXY=https://goproxy.io
 # 生成bin文件
 go build -o bin/gatewayDemo
 # 检查当前晋城市会否有gatewayDemo，如果有就杀死进程
-ps aux | grep gatewayDemo | grep -v 'grep' | awk '{print $2}' | xargs kill
+ps aux | grep gatewayDemo | grep -v 'grep' | awk '{print $2}' | xargs kill -9
 #
 nohup ./bin/gatewayDemo -conf=./conf/prod/ -endpoint=dashboard >>logs/dashboard.log 2>&1 &
 echo 'nohup ./bin/gatewayDemo -conf=./conf/prod/ -endpoint=dashboard >> logs/dashboard.log 2>&1 &'
